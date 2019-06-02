e9ea581a64ee9ed1beefc2b60ca25a53b80ea2e7
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-02 18:37:17 -0300

    Criação do componente Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './Modal.css';
+
+class Modal extends React.Component {
+
+	render() {
+		return(
+			<div id="myModal" className={this.props.show ? "modal show" : "modal"}>
+  			<div className="modal-content">
+    			<div className="modal-header">
+      			<span className="close">&times;</span>
+      			<h2>{this.props.title ? this.props.title : 'Modal'}</h2>
+    			</div>
+    			<div className="modal-body">
+      			{this.props.children}
+    			</div>
+    			<div className="modal-footer">
+      			<button>{this.props.submitLabel ? this.props.submitLabel : "Confirmar"}</button>
+    			</div>
+  			</div>
+			</div>
+		);
+	}
+
+}
+
+export default Modal;
