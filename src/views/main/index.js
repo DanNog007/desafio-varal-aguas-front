272be600a898379f583422579e9790c938da5036
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-02 19:24:05 -0300

    Reestruturação da view main/index, e metodos para exibir e ocultar modal

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -4,20 +4,48 @@ import Concurso from './concurso';
 import Sobre from './sobre';
 import Regras from './regras';
 import Footer from './footer';
+import Form from './form';
+import Modal from '../../components/Modal/Modal';
 import './css/index.css';
 
-function Index() {
-  return(
-    <div>
-      <Header />
-      <div className="Container">
-        <Concurso />
-        <Sobre />
-        <Regras />
+class Index extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: false
+    };
+
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+
+  showModal() {
+    this.setState({ showModal: true });
+  };
+
+  hideModal() {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    return(
+      <div>
+        <Header showModal={this.showModal} />
+        <div className="Container">
+          <Concurso showModal={this.showModal} />
+          <Sobre />
+          <Regras />
+        </div>
+        <Footer />
+        <Modal title="Cadastro" submitLabel="Enviar" show={this.state.showModal} hide={this.hideModal}>
+          <Form />
+        </Modal>
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
+
+
 
 export default Index;
