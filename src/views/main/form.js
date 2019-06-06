e9792fc35aa91b6e74f253475e0315dd5e063a3b
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-05 21:31:21 -0300

    Ajuste no formulario, usando o componente FormGroup

--- src/views/main/form.js
+++ src/views/main/form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/form.css';
+import FormGroup from '../../components/FromGroup/FormGroup';
 
 class Form extends React.Component{
 
@@ -7,31 +7,15 @@ class Form extends React.Component{
 		return(
       <form>
         <div style={{width:'48%', float:'left', margin:'0 1%'}}>
-          <div className="FormGroup">
-            <label for="nome">Nome do colaborador:</label><br/>
-            <input className="FormField" type="text" name="nome" id="nome" placeholder="Insira seu nome" />
-          </div>
-          <div className="FormGroup">
-            <label for="nomeFoto">Nome da foto:</label><br/>
-            <input className="FormField" type="text" name="nomeFoto" id="nomeFoto" placeholder="Insira o nome da foto" />
-          </div>
+          <FormGroup id="nome" label="Nome do colaborador:" type="text" placeholder="Insira seu nome" error="" />
+          <FormGroup id="nomeFoto" label="Nome da foto:" type="text" placeholder="Insira o nome da foto" error="" />
         </div>
         <div style={{width:'48%', float:'left', margin:'0 1%'}}>
-          <div className="FormGroup">
-            <label for="local">Local da foto:</label><br/>
-            <input className="FormField" type="text" name="local" id="local" placeholder="Insira o local da foto" />
-          </div>
-          <div className="FormGroup">
-            <label for="data">Data de registro:</label><br/>
-            <input className="FormField" type="text" name="data" id="data" placeholder="DD/MM/AAAA" />
-          </div>
+          <FormGroup id="local" label="Local da foto:" type="text" placeholder="Insira o local da foto" error="" />
+          <FormGroup id="data" label="Data de registro:" type="text" placeholder="DD/MM/AAAA" error="" />
         </div>
-        <div className="FormGroup" style={{clear:'both'}}>
-          <label for="foto">Anexar Foto:</label><br/>
-          <input type="file" name="foto" id="foto" />
-        </div>
-        <div className="FormGroup" style={{clear:'both'}}>
-          <label>Termos:</label><br/>
+        <FormGroup id="imageFile" label="Anexar Foto:" type="file" error="" />
+        <FormGroup label="Termos:" type="none" error="">
           <div style={{height:'200px', width:'100%', overflow:'auto', border:'solid 1px #e5e5e5'}}>
             <ol type="I" style={{color:'#777777', fontSize:'11pt', paddingLeft:'26px'}}>
               <li style={{marginBottom:'15px'}}>Confirmo autoria da fotografia, que não constitui plágio em espécie alguma e não possui manipulação digital ou sobre posição;</li>
@@ -42,13 +26,8 @@ class Form extends React.Component{
               <li style={{marginBottom:'15px'}}>Autorização para veiculação de imagem e nome do colaborador, nome artistico para a foto, local e data de registro da foto, bem como permissão para os organizadores, a critério próprio, sem limite de tempo e sem a incidência de quaisquer ônus utilizarem ou divulgarem as imagens inscritas;</li>
             </ol>
           </div>
-        </div>
-        <div className="FormGroup" style={{clear:'both'}}>
-          <label className="LabelCheckbox" for="termo">
-            <input type="checkbox" name="termo" id="termo" />
-            Eu li e concordo com os termos
-          </label>
-        </div>
+        </FormGroup>
+        <FormGroup id="termo" label="Eu li e concordo com os termos" type="checkbox" error="" />
       </form>
 		);
 	}
