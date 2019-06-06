b657ba47bea1c24a3b78b1ee35eb855b9714d89c
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-05 21:29:54 -0300

    Criação de componente para os formularios

---
+++ src/components/FromGroup/FormGroup.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './FormGroup.css';
+
+class FormGroup extends React.Component{
+	render(){
+    if (this.props.type == "text"){
+      return(
+        <div className="FormGroup">
+          <label for={this.props.id}>{this.props.label}</label>
+          <input className="FormField" type="text" name={this.props.id} id={this.props.id} placeholder={this.props.placeholder} />
+          <label className={this.props.error ? "FormErrorShow" : "FormError"}>{this.props.error}</label>
+        </div>
+      );
+    } else if (this.props.type == "file"){
+      return(
+        <div className="FormGroup">
+          <label for={this.props.id}>{this.props.label}</label>
+          <input type="file" name={this.props.id} id={this.props.id} />
+          <label className={this.props.error ? "FormErrorShow" : "FormError"}>{this.props.error}</label>
+        </div>
+      );
+    } else if (this.props.type == "checkbox"){
+      return(
+        <div className="FormGroup">
+          <label className="LabelCheckbox" for={this.props.id}>
+            <input type="checkbox" name={this.props.id} id={this.props.id} />
+          {this.props.label}</label>
+          <label className={this.props.error ? "FormErrorShow" : "FormError"}>{this.props.error}</label>
+        </div>
+      );
+    } else {
+      return(
+        <div className="FormGroup">
+          <label for={this.props.id}>{this.props.label}</label>
+            {this.props.children}
+            <label className={this.props.error ? "FormErrorShow" : "FormError"}>{this.props.error}</label>
+        </div>
+      );
+    }
+  }
+}
+
+export default FormGroup;
