3614b8859912e2ccbb32ff5c034d5bb385eaf1e4
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-06 15:12:53 -0300

    Ajustes no formulário, e requisição de incrição para a API

--- src/views/main/form.js
+++ src/views/main/form.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import FormGroup from '../../components/FromGroup/FormGroup';
+import FormGroup from '../../components/FormGroup/FormGroup';
 
 class Form extends React.Component{
 
 	render(){
 		return(
-      <form>
+      <form id={this.props.id}>
         <div style={{width:'48%', float:'left', margin:'0 1%'}}>
-          <FormGroup id="nome" label="Nome do colaborador:" type="text" placeholder="Insira seu nome" error="" onChange={this.props.handleInputChange} />
-          <FormGroup id="nome_foto" label="Nome da foto:" type="text" placeholder="Insira o nome da foto" error="" onChange={this.props.handleInputChange} />
+          <FormGroup id="nome" label="Nome do colaborador:" type="text" placeholder="Insira seu nome" error={this.props.error.nome} onChange={this.props.handleInputChange} />
+          <FormGroup id="nome_foto" label="Nome da foto:" type="text" placeholder="Insira o nome da foto" error={this.props.error.nome_foto} onChange={this.props.handleInputChange} />
         </div>
         <div style={{width:'48%', float:'left', margin:'0 1%'}}>
-          <FormGroup id="local_foto" label="Local da foto:" type="text" placeholder="Insira o local da foto" error=""  onChange={this.props.handleInputChange}/>
-          <FormGroup id="data_foto" label="Data de registro:" type="text" placeholder="DD/MM/AAAA" error="" onChange={this.props.handleInputChange} />
+          <FormGroup id="local_foto" label="Local da foto:" type="text" placeholder="Insira o local da foto" error={this.props.error.local_foto}  onChange={this.props.handleInputChange}/>
+          <FormGroup id="data_foto" label="Data de registro:" type="date" placeholder="DD/MM/AAAA" error={this.props.error.data_foto} onChange={this.props.handleInputChange} />
         </div>
-        <FormGroup id="imageFile" label="Anexar Foto:" type="file" error="" onChange={this.props.handleInputChange} />
+        <FormGroup id="imageFile" label="Anexar Foto:" type="file" error={this.props.error.foto} onChange={this.props.handleInputChange} />
         <FormGroup label="Termos:" type="none" error="">
           <div style={{height:'200px', width:'100%', overflow:'auto', border:'solid 1px #e5e5e5'}}>
             <ol type="I" style={{color:'#777777', fontSize:'11pt', paddingLeft:'26px'}}>
@@ -27,7 +27,7 @@ class Form extends React.Component{
             </ol>
           </div>
         </FormGroup>
-        <FormGroup id="termo" label="Eu li e concordo com os termos" type="checkbox" error="" onChange={this.props.handleInputChange} />
+        <FormGroup id="termos" label="Eu li e concordo com os termos" type="checkbox" error={this.props.error.termos} onChange={this.props.handleInputChange} />
       </form>
 		);
 	}
--- src/views/main/index.js
+++ src/views/main/index.js
@@ -6,6 +6,7 @@ import Regras from './regras';
 import Footer from './footer';
 import Form from './form';
 import Modal from '../../components/Modal/Modal';
+import Toast from '../../components/Toast/Toast';
 import './css/index.css';
 
 class Index extends React.Component{
@@ -17,52 +18,77 @@ class Index extends React.Component{
       colaborador: {},
       error: {},
       formData: new FormData(),
-      response: {},
+      showToast: false,
+      message: "",
     };
 
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showToast = this.showToast.bind(this);
   }
 
   showModal() {
-    this.setState({ showModal: true });
+    document.getElementById("inscricao").reset();
+    this.setState({ showModal: true, colaborador: {}, error: {}, formData: new FormData() });
   };
 
   hideModal() {
-    this.setState({ showModal: false });
+    this.setState({ showModal: false, colaborador: {}, error: {}, formData: {} });
+    document.getElementById("inscricao").reset();
   };
+
+  showToast(){
+    this.setState({showToast: true});
+    setTimeout(() =>{this.setState({showToast: false})}, 3000);
+  }
 
   handleInputChange(event) {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === 'checkbox' ? (target.checked ? 1 : 0) : target.value;
     const name = target.name;
+
     let formData = this.state.formData;
     let colaborador = this.state.colaborador;
+    let error = this.state.error;
 
     if (target.type === 'file'){
       formData.append('imageFile', target.files[0]);
+      error.foto = "";
     } else {
       colaborador[name] = value;
-      formData.append(name, value);
+      error[name] = "";
     }
 
     this.setState({
       colaborador: colaborador,
       formData: formData,
+      error: error,
     });
     console.log(this.state);
   }
 
   handleSubmit(){
+    let formData = this.state.formData;
+    let colaborador = this.state.colaborador
+    Object.keys(colaborador).forEach(key => formData.append(key, colaborador[key]));
+
     fetch('http://localhost:8080/colaborador',  {
       method: 'POST',
-      body: this.state.formData
+      body: formData
     })
       .then(response => response.json())
-      .then(data => this.setState({ response:data }));
-    console.log(this.state);
+      .then(data => {
+        if (data.status === true){
+          this.setState({message: "\u2713 Inscrição realizada com sucesso!"});
+          this.showToast();
+          this.hideModal();
+          document.getElementById("inscricao").reset();
+        } else {
+          this.setState({ error: data.error});
+        }
+      });
   }
 
   render() {
@@ -76,13 +102,12 @@ class Index extends React.Component{
         </div>
         <Footer />
         <Modal title="Cadastro" submitLabel="Enviar" show={this.state.showModal} hide={this.hideModal} action={this.handleSubmit}>
-          <Form handleInputChange={this.handleInputChange} />
+          <Form id="inscricao" handleInputChange={this.handleInputChange} error={this.state.error} />
         </Modal>
+        <Toast show={this.state.showToast} message={this.state.message} />
       </div>
     );
   }
 }
-
-
 
 export default Index;
