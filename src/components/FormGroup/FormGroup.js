d9ffaa7b7a1e70c0801b359ac594c11b56d4e8ef
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-06 14:28:24 -0300

    Inclusão do tipo data no FormGroup

--- src/components/FormGroup/FormGroup.js
+++ src/components/FormGroup/FormGroup.js
@@ -11,6 +11,14 @@ class FormGroup extends React.Component{
           <label className={this.props.error ? "FormErrorShow" : "FormError"}>{this.props.error}</label>
         </div>
       );
+    } else if (this.props.type == "date"){
+      return(
+        <div className="FormGroup">
+          <label for={this.props.id}>{this.props.label}</label>
+          <input className="FormField" type="date" name={this.props.id} id={this.props.id} placeholder={this.props.placeholder} onChange={this.props.onChange} />
+          <label className={this.props.error ? "FormErrorShow" : "FormError"}>{this.props.error}</label>
+        </div>
+      );
     } else if (this.props.type == "file"){
       return(
         <div className="FormGroup">
